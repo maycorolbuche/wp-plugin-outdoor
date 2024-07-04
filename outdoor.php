96f52358467de84b8846a49a4c57ab9680a21734
maycorolbuche <mayco_rolbuche@hotmail.com>
2024-07-04 08:57:10 -0300

    Ajuste Versão

--- outdoor.php
+++ outdoor.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * @package Outdoor
- * @version 1.3.0
+ * @version 1.3.1
  */
 /*
 Plugin Name: Outdoor
 Plugin URI: https://wordpress.org/plugins/outdoor/
 Description: Projetar vídeos e imagens em looping de forma dinâmica.
-Version: 1.3.0
+Version: 1.3.1
 Requires at least: 5.0
 Requires PHP: 7.0
 Author: Mayco Rolbuche
